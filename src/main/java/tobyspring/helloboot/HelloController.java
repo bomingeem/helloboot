22d43caa3ce2247ce750b37460b3aa5dd0260308
bomingeem <bomingeem@gmail.com>
2023-01-23 11:48:20 +0900

    Bean의 생명주기 메소드

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,8 +1,7 @@
 package tobyspring.helloboot;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
@@ -10,9 +9,13 @@ import java.util.Objects;
 @RestController
 public class HelloController {
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println(applicationContext);
     }
 
     @GetMapping("/hello")
